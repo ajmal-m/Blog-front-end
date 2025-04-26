7776f309afddac25a1a2ee2fd19864609f95da26
ajmal5410347 <ajmal@airtory.com>
2025-04-26 22:18:03 +0530

    Editor Header added

--- src/component/Editor/Navbar.tsx
+++ src/component/Editor/Navbar.tsx
@@ -1,8 +1,9 @@
 export default function Navbar() {
   return (
     <>
-        <div className="w-full h-[100px] bg-[red]">
-            Header
+        <div className="w-full h-[100px] bg-[#558ef3] dark:bg-[#558ef3] py-[6px] flex items-center justify-end">
+          <button className="cursor-pointer bg-[black] rounded text-[white] px-[8px] py-[4px] text-[12px] font-[500]">Post</button>
+          <button className="cursor-pointer bg-[black] rounded text-[white] px-[8px] py-[4px] text-[12px] font-[500] m-1">Preview</button>
         </div>
     </>
   )
