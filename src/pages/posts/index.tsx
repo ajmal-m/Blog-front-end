f4ecd2c1b24e85e701b24ee034f51ba7a2e09e3e
ajmal-m <ajua76774@gmail.com>
2025-05-07 20:49:47 -0700

    Empty ui added

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -8,12 +8,15 @@ import { UseTheme } from "../../hooks/themeContext";
 export default function Posts() {
 
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
     const {theme} = UseTheme();
 
     useEffect(() => {
         const fetchPosts = async() => {
+            setLoading(true);
             const data = await getPosts();
             setPosts(data?.posts)
+            setLoading(false);
         }
 
         fetchPosts();
@@ -22,12 +25,16 @@ export default function Posts() {
         <>
             <div className={`grid grid-cols-4 gap-4 flex-wrap overflow-y-auto h-auto p-[16px] ${theme === 'dark' ? 'bg-[black]' : 'bg-[white]'}`} >
                 {
-                    posts.length ? (
+                    !loading && posts.length ? (
                         posts.map((item: Post) => (
                            <PostCard post={item} key={item._id}/>
                         ))
-                    ) : (
+                    ) : loading && !posts.length ? (
                         <Loader/>
+                    ):(
+                        <div className={`h-screen w-screen flex items-center justify-center ${theme === 'dark' ? 'text-white' : 'text-black'}`}>
+                            Empty
+                        </div>
                     )
                 }
                 
