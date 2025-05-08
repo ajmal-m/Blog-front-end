5b94863d850b37b8cf40671940d64a67b529863a
ajmal-m <ajua76774@gmail.com>
2025-05-08 03:25:30 -0700

    Post card update

--- src/component/PostCard.tsx
+++ src/component/PostCard.tsx
@@ -5,46 +5,41 @@ export default function PostCard({ post }: {post : Post}) {
     console.log(post)
   return (
    <>
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-active-color">
-            {post.title}
+    <div className="max-w-sm h-[200px] p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex flex-col">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-active-color h-[96px] overflow-hidden">
+                {post.title}
             </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 max-h-[60px] truncate">
-            {post.description}
-        </p>
-        <NavLink to={`/post/${post._id}`} 
-            className='
-                inline-flex items-center px-3 py-2 
-                text-sm font-medium text-center text-white bg-blue-700 
-                rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
-                focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 
-                dark:focus:ring-blue-800
-            '
-        >
-            Read more
-            <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-            >
-            <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-            </svg>
-        </NavLink>
+            <div className="flex items-center gap-4 mt-[12px]">
+                <NavLink to={`/post/${post._id}`} 
+                    className='
+                        inline-flex items-center px-3 py-2 
+                        text-sm font-medium text-center text-white bg-blue-700 
+                        rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
+                        focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 
+                        dark:focus:ring-blue-800
+                    '
+                >
+                    Read more
+                    <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                    >
+                    <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                    </svg>
+                </NavLink>
+            </div>
+        </div>
     </div>
-
-    {/* <div className="w-[318px] h-[412px] bg-[#1c1f26] dark:bg-[#1c1f26] rounded-[16px] border border-[#a8b3cf] dark:border-[#a8b3cf] p-[8px]">
-
-    </div> */}
    </>
   )
 }
