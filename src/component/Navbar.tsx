d4a672780853f31dc0b0e6d93d0572a6a84236e2
ajmal-m <ajua76774@gmail.com>
2025-05-08 00:20:23 -0700

    Dark Mode update

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,12 +1,14 @@
-import { NavLink, useNavigate } from "react-router";
+import { NavLink, useLocation, useNavigate } from "react-router";
 import { useAuth } from "../hooks/authContext";
 import { UseTheme } from "../hooks/themeContext";
 import { DrawerMenu } from "./Drawer";
+import {ThemeToggle} from '../../@/components/tiptap-templates/simple/theme-toggle'
 
 const Navbar = () => {
   const {user, logOut} = useAuth();
   const navigate = useNavigate();
   const {theme, updateTheme} = UseTheme();
+  const location = useLocation();
 
   const handleLogout = () => {
     logOut();
@@ -53,11 +55,20 @@ const Navbar = () => {
                 </li>
               )
             }
-            <li>
-              <button onClick={ () => updateTheme( theme === 'dark' ? 'light' : 'dark' )} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                { theme === 'dark' ? 'Light' : 'Dark'}
-              </button>
-            </li>
+           {
+            !location.pathname.includes("/post/") && (
+              <li>
+                <button onClick={ () => updateTheme( theme === 'dark' ? 'light' : 'dark' )} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  { theme === 'dark' ? 'Light' : 'Dark'}
+                </button>
+              </li>
+            )
+           }
+            {location.pathname.includes("/post/") && (
+              <li>
+                  <ThemeToggle/>
+              </li>
+            )}
           </ul>
         </div>
       </div>
