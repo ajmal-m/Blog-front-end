7bdcd8a6177a1251de781cda5e99c7114d77bf49
ajmal-m <ajua76774@gmail.com>
2025-05-09 23:54:49 -0700

    Drawer closing added

--- src/component/Drawer.tsx
+++ src/component/Drawer.tsx
@@ -36,7 +36,7 @@ export function DrawerMenu() {
               </button>
                {
             !location.pathname.includes("/post/") && (
-                <button onClick={ () => updateTheme( theme === 'dark' ? 'light' : 'dark' )} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button onClick={ () =>{ updateTheme( theme === 'dark' ? 'light' : 'dark' ); setIsOpen(false); }} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   { theme === 'dark' ? 'Light' : 'Dark'}
                 </button>
             )
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white dark:bg-gray-900 w-full z-20 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-start mx-auto p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
