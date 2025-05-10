2bb885e4d1fb6c20191263125e0c5654cb3b0ba1
ajmal-m <ajua76774@gmail.com>
2025-05-09 22:28:46 -0700

    Drawer ui update

--- src/component/Drawer.tsx
+++ src/component/Drawer.tsx
@@ -12,7 +12,9 @@ export function DrawerMenu() {
   return (
     <>
       <div className="flexitems-center justify-center">
-        <Button className="cursor-pointer bg-[red]" onClick={() => setIsOpen(true)}>Show drawer</Button>
+        <svg onClick={() => setIsOpen(true)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-menu-icon lucide-menu cursor-pointer">
+          <path d="M4 12h16"/><path d="M4 18h16"/><path d="M4 6h16"/>
+        </svg>
       </div>
       <Drawer open={isOpen} onClose={handleClose}>
         <DrawerItems>
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -23,7 +23,6 @@ const Navbar = () => {
           id="navbar-sticky"
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <DrawerMenu/>
             <li className="flex items-center">
               <NavLink 
                 to={'/'}
@@ -70,6 +69,9 @@ const Navbar = () => {
               </li>
             )}
           </ul>
+        </div>
+        <div className="hidden max-sm:flex">
+          <DrawerMenu/>
         </div>
       </div>
     </nav>
