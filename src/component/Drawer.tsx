6f6874f48610c70c9f5f8bd6af877119f5554036
ajmal-m <ajua76774@gmail.com>
2025-05-19 15:38:06 +0530

    Drawer user image update

--- src/component/Drawer.tsx
+++ src/component/Drawer.tsx
@@ -8,6 +8,8 @@ import { NavLink, useNavigate } from "react-router";
 import { logOutUser } from "../store/userSlice";
 import { toggleTheme } from "../store/themeSlice";
 import { RootStore } from "../store";
+import { Avatar } from "flowbite-react";
+
 
 export function DrawerMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,8 +36,15 @@ export function DrawerMenu() {
       <Drawer open={isOpen} onClose={handleClose} className="transition-all duration-300 ease-in-out bg-[red]">
         <DrawerItems>
           <div className="flex flex-col gap-4">
-             <button type="button" onClick={() => { setIsOpen(false); navigate('/user')} } className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                { user ? user.name : 'Guest'}
+            <div className="flex items-center gap-1 justify-center">
+              <Avatar img={user?.avatar ?? ""} size="md"  rounded/>
+              <p className="text-white text-[12px] font-[500]">{user?.name ?? "User"}</p>
+            </div>
+             <button type="button" onClick={() => { setIsOpen(false); navigate('/user')} } 
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 
+              font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                { 'Edit Profile'}
               </button>
                {
             !location.pathname.includes("/post/") && (
